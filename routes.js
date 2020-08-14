45ad2fb45ce88bdb1fe343e53c9754cc8928373b
oirase <cashewnutmuffin@outlook.com>
2020-08-15 07:41:11 +0900

    all

---
+++ routes.js
@@ -0,0 +1,41 @@
+"use strict"
+
+const express = require('express')
+const fs = require('fs')
+const ejs = require('ejs')
+const path = require('path')
+//const cookieParser = require('cookie-parser')
+//const session = require('express-session')
+const app = express()
+
+app.engine('ejs', ejs.renderFile)
+//const indexPage = fs.readFileSync('index.html', 'utf-8')
+//app.use(express.static(path.join(__dirname, 'public')))
+//app.use(cookieParser())
+/*
+app.use(session({
+    secret: 'hoge',
+    resave: true,
+  saveUninitialized: true
+}))
+*/
+app.get('/', (req, res) => {
+  //let cnt = req.session.cnt == undefined ? 0 : req.session.cnt
+  const cnt = 5
+  //cnt++
+  //req.session.cnt = cnt
+  res.render('temp.ejs', { cnt })
+})
+
+/*
+app.get('/', (req, res) => {
+  let cnt = req.cookies.cnt == undefined ? 0 : req.cookies.cnt
+  cnt++
+  res.cookie('cnt', cnt, {maxAge: 5000})
+  res.render('temp.ejs', { cnt })
+})
+*/
+
+const server = app.listen(1234, ()=>{
+  console.log('server')
+})
