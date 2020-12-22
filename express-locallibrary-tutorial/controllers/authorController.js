8fa53acd0c61af46d36644699fb823fff7664eff
oirase <cashewnutmuffin@outlook.com>
2020-12-22 22:28:15 +0900

    express-locallibrary-tutorial/

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -4,6 +4,10 @@ exports.author_list = function(req, res) {
   res.send('NOT IMPLEMENTED: Author list')
 }
 
+exports.author_detail = function(req, res) {
+  res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id)
+}
+
 exports.author_create_get = function(req, res) {
   res.send('NOT IMPLEMENTED: Author create GET')
 }
