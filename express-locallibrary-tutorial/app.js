7dc55d910b036d245ae0bb9f4f51fa7b0fdf37b3
oirase <cashewnutmuffin@outlook.com>
2020-12-22 22:56:56 +0900

    express-locallibrary-tutorial/

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -10,12 +10,13 @@ var catalogRouter = require('./routes/catalog')
 
 var app = express();
 var mongoose = require('mongoose');
+/*
 var mongoDB = 'mongodb://0.0.0.0';
 mongoose.connect(mongoDB,  { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+*/
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
