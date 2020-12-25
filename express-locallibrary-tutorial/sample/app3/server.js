271c2e21d9a57b2fc0f54a2f3a099f5d71c8c63c
oirase <cashewnutmuffin@outlook.com>
2020-12-26 05:29:33 +0900

    express-locallibrary-tutorial/

---
+++ express-locallibrary-tutorial/sample/app3/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose');
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+mongoDB = 'mongodb://0.0.0.0/study';
+mongoose.connect(mongoDB,  { useNewUrlParser: true, useUnifiedTopology: true }))
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+s
+var Schema = mongoose.Schema;
+var BookSchema = new Schema({
+    name: {type: String, required: true},
+})
+const Book = mongoose.model('Book', BookSchema);
+const artbook = new Book({ name: 'artbook' })
+artbook.save().then(() => console.log('success'))
+
+app.use('/', function(res,req) {
+  res.render('index', { count: artbook.countDocuments })
+})
