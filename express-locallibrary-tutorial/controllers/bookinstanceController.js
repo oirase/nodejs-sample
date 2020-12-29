2b517068392414ecd2847842771ae3b49bc785f6
oirase <cashewnutmuffin@outlook.com>
2020-12-29 13:30:33 +0900

    express-locallibrary-tutorial/

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -4,7 +4,7 @@ var BookInstance = require('../models/bookinstance')
 exports.bookinstance_list = function(req, res, next) {
     BookInstance.find()
       .populate('book')
-      .exec(function (err, list_bookinstance) {
+      .exec(function (err, list_bookinstances) {
         if (err) { return next(err) }
         res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances })
       })
