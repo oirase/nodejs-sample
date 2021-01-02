04355475e81ee6c31aadbf7639c26c9c23c9b646
oirase <cashewnutmuffin@outlook.com>
2021-01-02 10:16:14 +0900

    express-locallibrary-tutorial/

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -1,6 +1,7 @@
 var Genre = require('../models/genre')
 var Book = require('../models/book');
 var async = require('async');
+const { body, validationResult } = require("express-validator")
 
 // Display list of all Genre.
 exports.genre_list = function(req, res, next) {
@@ -38,14 +39,36 @@ exports.genre_detail = function(req, res, next) {
 };
 
 // Display Genre create form on GET.
-exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+exports.genre_create_get = function(req, res, next) {
+    res.render('genre_form', { title: 'Create genre' })
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+  body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+  ], (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      res.render('genre_form', { title: 'Create Genre', genre: genre, errors.array()})
+      return
+    }
+    else {
+      Genre.findOne({ 'name': req.body.name })
+      .exec( function(err, found_genre) {
+        if(err) { return next(err) }
+
+        if(found_genre) {
+          res.redirect(found_genre.url)
+        }
+        else {
+          genre.save(function (err) {
+            if (err) { return next(err) }
+              res.redirect(genre.url)
+          })
+        }
+      })
+    }
+
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
@@ -66,3 +89,4 @@ exports.genre_update_get = function(req, res) {
 exports.genre_update_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Genre update POST');
 };
+[[]]
