763fe6ffb6d7e1a060f365f0eb4d767e8298261c
oirase <cashewnutmuffin@outlook.com>
2021-01-07 15:21:57 +0900

    express-validator

--- express-validator-sample/controllers/userController.js
+++ express-validator-sample/controllers/userController.js
@@ -8,10 +8,11 @@ exports.index_post = [
 body('username').isEmail(),
 body('password').isLength({ min: 5 }),
 function(req, res, next){
-  const erros = validationResult(req)
-    if (!erors.isEmpty()) {
+  const errors = validationResult(req)
+    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
+    res.send('success!!')
   }
 ]
 
