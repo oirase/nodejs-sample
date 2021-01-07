1048611939390f25b99cc072cf1b8a5cbbb28b78
oirase <cashewnutmuffin@outlook.com>
2021-01-07 13:29:28 +0900

    express-validator/

--- express-validator/controllers/userController.js
+++ express-validator/controllers/userController.js
@@ -1,8 +1,17 @@
+const { body, validationResult }　＝　require('express-validator')
 
 exports.index= function(req, res, next){
   res.render('index')
 }
 
-exports.index_post = function(req, res, next){
+exports.index_post = [
+body('username').isEmail(),
+body('password').isLength({ min: 5 }),
+function(req, res, next){
+  const erros = validationResult(req)
+    if (!erors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+  }
+]
 
-}
